7bf3bd61cb49d01e1a535d887bccabfd018f266a
youngff43 <youngff43@gmail.com>
2022-06-04 12:33:45 -0400

    added voting method

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
 
-// get all posts
+// GET /api/posts
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         order: [['created_at', 'DESC']], 
         include: [
             {
@@ -21,13 +22,13 @@ router.get('/', (req, res) => {
     });
   });
 
-// get a single post 
+// GET /api/posts/:id
 router.get('/:id', (req, res) => {
     Post.findOne({
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
       include: [
         {
           model: User,
@@ -62,6 +63,17 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+// PUT /api/posts/upvote
+    router.put('/upvote', (req, res) => {
+        // custom static method created in models/Post.js
+        Post.upvote(req.body, { Vote })
+          .then(updatedPostData => res.json(updatedPostData))
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      });
 
 //updating a posts table 
 router.put('/:id', (req, res) => {
@@ -107,5 +119,6 @@ router.delete('/:id', (req, res) => {
         res.status(500).json(err);
       });
   });
+
 module.exports = router;
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote  } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -14,13 +14,25 @@ router.get('/', (req, res) => {
     });
   });
 
-// GET /api/users/1
+// GET /api/users/:id
 router.get('/:id', (req, res) => {
 User.findOne({
-  attributes: { exclude: ['password'] },
   where: {
     id: req.params.id
-  }
+  },
+  attributes: { exclude: ['password'] },
+  include: [
+    {
+      model: Post,
+      attributes: ['id', 'title', 'post_url', 'created_at']
+    },
+    {
+      model: Post,
+      attributes: ['title'],
+      through: Vote,
+      as: 'voted_posts'
+    }
+  ]
 })
   .then(dbUserData => {
     if (!dbUserData) {

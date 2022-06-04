ebe655d575dd8b6f896a858cb56d4757cab2fabe
youngff43 <youngff43@gmail.com>
2022-06-04 13:33:48 -0400

    added comment routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 // GET /api/posts
@@ -9,6 +9,14 @@ router.get('/', (req, res) => {
         attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         order: [['created_at', 'DESC']], 
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                  }             
+            },
             {
               model: User,
               attributes: ['username']
@@ -30,6 +38,14 @@ router.get('/:id', (req, res) => {
       },
       attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
       include: [
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+              }             
+        },
         {
           model: User,
           attributes: ['username']
@@ -100,7 +116,7 @@ router.put('/:id', (req, res) => {
       });
   });
 
-  // delete a post 
+// delete a post 
 router.delete('/:id', (req, res) => {
     Post.destroy({
       where: {
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote  } = require('../../models');
+const { User, Post, Vote, Comment  } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -25,6 +25,14 @@ User.findOne({
     {
       model: Post,
       attributes: ['id', 'title', 'post_url', 'created_at']
+    },
+    {
+      model: Comment,
+      attributes: ['id', 'comment_text', 'created_at'],
+      include: {
+        model: Post,
+        attributes: ['title']
+      }
     },
     {
       model: Post,
